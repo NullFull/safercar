1d4b8174d2e69fecfdd659868542e1ecbfb23731
cmygray <cmygray@gmail.com>
2018-03-04 18:02:37 +0900

    feat: 활성화된 탭에 해당하는 결함 목록만 표시한다

---
+++ desucar/js/script.js
@@ -0,0 +1,87 @@
+const baseUrl = 'http://127.0.0.1:8000'
+const searchbar = document.querySelector('.q')
+const autocomplete = document.querySelector('.autocomplete')
+const tabs = document.querySelector('.navbar-tabs')
+const defects = document.querySelectorAll('.defect')
+
+let cars = []
+let currentTab
+
+const toggleTab = (tab) => {
+  if (tab.id !== currentTab.id) {
+    currentTab.classList.remove('active')
+    currentTab = tab
+    currentTab.classList.add('active')
+    renderDefects(currentTab.id)
+  }
+}
+
+const renderDefects = (id) => {
+  defects.forEach(d => {
+    d.style.display = d.className.includes(id) ? '' : 'none'
+  })
+}
+
+if (tabs) {
+  currentTab = Array.from(tabs.children).find(item => !item.classList.contains('no-value'))
+  currentTab.classList.add('active')
+  renderDefects(currentTab.id)
+
+  tabs.addEventListener('click', e => {
+    if (e.target.nodeName === 'LI') {
+      toggleTab(e.target)
+    }
+  })
+}
+
+if (searchbar) {
+  const renderSuggestion = () => {
+    autocomplete.innerHTML = ''
+    cars.forEach(car => {
+      autocomplete.innerHTML += `
+          <li class="dropdown-item">
+            <a href="/${car.fields.maker}/${car.fields.simple_name}-${car.fields.make_start.substr(0, 4)}-${car.fields.code}">${car.fields.name}</a>
+          </li>
+          `
+    })
+  }
+
+  const debounce = (func, wait, immediate) => {
+    let timeout
+    return function () {
+      const context = this
+      const args = arguments
+      const later = () => {
+        timeout = null
+        if (!immediate) func.apply(context, args)
+      }
+      const callNow = immediate && !timeout
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+      if (callNow) func.apply(context, args)
+    }
+  }
+
+  const getSuggestion = debounce(() => {
+    if (!searchbar.value) {
+      cars = []
+      renderSuggestion()
+    } else {
+      return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest()
+        req.open('GET', `${baseUrl}/suggest?q=${searchbar.value}`)
+        req.send()
+        req.onreadystatechange = function () {
+          if (req.readyState === XMLHttpRequest.DONE) {
+            if (req.status === 200) {
+              cars = JSON.parse(req.response)
+              renderSuggestion()
+            }
+          }
+        }
+      })
+    }
+  }, 300)
+
+  searchbar.addEventListener('input', getSuggestion)
+}
--- desucar/static/script.js
+++ desucar/static/script.js
@@ -1,53 +1,85 @@
-const baseUrl = 'http://127.0.0.1:8000'
-const searchbar = document.querySelector('.q')
-const dropdown = document.querySelector('.dropdown')
-const autocomplete = document.querySelector('.autocomplete')
+var baseUrl = 'http://127.0.0.1:8000';
+var searchbar = document.querySelector('.q');
+var autocomplete = document.querySelector('.autocomplete');
+var tabs = document.querySelector('.navbar-tabs');
+var defects = document.querySelectorAll('.defect');
 
-let cars = [];
+var cars = [];
+var currentTab = void 0;
 
-const render = () => {
-    autocomplete.innerHTML = '';
-    cars.forEach(car => {
-        autocomplete.innerHTML += `
-        <li class="dropdown-item"><a href="/${car.fields.maker}/${car.fields.simple_name}-${car.fields.make_start.substr(0, 4)}-${car.fields.code}">${car.fields.name}</a></li>
-        `            
-    })
-}
-
-const debounce = (func, wait, immediate) => {
-    let timeout;
-    return function() {
-        const context = this, args = arguments;
-        const later = () => {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        const callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
+var toggleTab = function toggleTab(tab) {
+  if (tab.id !== currentTab.id) {
+    currentTab.classList.remove('active');
+    currentTab = tab;
+    currentTab.classList.add('active');
+    renderDefects(currentTab.id);
+  }
 };
 
-const getSuggestion = debounce(() => {
-    if (!searchbar.value) {
-        cars = [];
-        render();
+var renderDefects = function renderDefects(id) {
+  defects.forEach(function (d) {
+    d.style.display = d.className.includes(id) ? '' : 'none';
+  });
+};
+
+if (tabs) {
+  currentTab = Array.from(tabs.children).find(function (item) {
+    return !item.classList.contains('no-value');
+  });
+  currentTab.classList.add('active');
+  renderDefects(currentTab.id);
+
+  tabs.addEventListener('click', function (e) {
+    if (e.target.nodeName === 'LI') {
+      toggleTab(e.target);
     }
-    return new Promise((res, rej) => {
-        const req = new XMLHttpRequest();
-        req.open('GET', `${baseUrl}/suggest?q=${searchbar.value}`);
-        if (searchbar.value)
+  });
+}
+
+if (searchbar) {
+  var renderSuggestion = function renderSuggestion() {
+    autocomplete.innerHTML = '';
+    cars.forEach(function (car) {
+      autocomplete.innerHTML += '\n          <li class="dropdown-item">\n            <a href="/' + car.fields.maker + '/' + car.fields.simple_name + '-' + car.fields.make_start.substr(0, 4) + '-' + car.fields.code + '">' + car.fields.name + '</a>\n          </li>\n          ';
+    });
+  };
+
+  var debounce = function debounce(func, wait, immediate) {
+    var timeout = void 0;
+    return function () {
+      var context = this;
+      var args = arguments;
+      var later = function later() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  var getSuggestion = debounce(function () {
+    if (!searchbar.value) {
+      cars = [];
+      renderSuggestion();
+    } else {
+      return new Promise(function (resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', baseUrl + '/suggest?q=' + searchbar.value);
         req.send();
         req.onreadystatechange = function () {
-            if (req.readyState === XMLHttpRequest.DONE) {
-                if (req.status === 200) {
-                    cars = JSON.parse(req.response)
-                    render()
-                }
+          if (req.readyState === XMLHttpRequest.DONE) {
+            if (req.status === 200) {
+              cars = JSON.parse(req.response);
+              renderSuggestion();
             }
+          }
         };
-    })
-}, 300)
+      });
+    }
+  }, 300);
 
-searchbar.addEventListener('input', getSuggestion)
+  searchbar.addEventListener('input', getSuggestion);
+}
