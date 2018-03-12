970ce0804888b8c7692aa1371f551d707b297d50
arai-youn <breezeshawl@gmail.com>
2018-03-12 21:23:35 +0900

    2018. 3. 12 이미지 경로 수정

--- desucar/settings.py
+++ desucar/settings.py
@@ -90,3 +90,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 NORMALIZE_MAP_PATH = os.path.join(BASE_DIR, 'data', 'normalize_map.json')
 
+GSPREAD_AUTH = {
+    "type": "service_account",
+    "project_id": "newstapa-web",
+    "private_key_id": "097a038b83cbfeb0fbd673670852d07299ea13b1",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDhgGMD/YbwqZ9r\n2YQtkHVI59iiRjqXrQBa4xLlmDqEQOYX9OH5FDHCQ37Zdg9rFlmieKTqI/4n0gqw\nqw/imARRoSzFVEJDrn3WytDLKrYI4KlTu1eWw0bf7Ie2i4Z4EwlwbS53ZhRptf58\nYGFVg5r2OrloCKwjGaxxtJ1c8BKc/5fLvEIiZ2SxitHtr5Ao9mpH1cqgIv0kkS2o\nkfZE0phQ9p/SSE0YqpsEfrIbY0XRthDWpvlI4RUGAtJ5G999/5k9Gb2JLorVb/xC\ntJRtcP++ly2WbCvu8RJ95C3zoGZNJf9NiFzka1hm32P8FFNVc8t8TPwjVw1h+tev\nDo6ibmP/AgMBAAECggEAKrkYyVpAtRrl7dh9nuZWGjR2yjPVCQSJYy7PVzlyqLjk\n/xfxsi+dx8ji4Q9pl2faHpqN4CBmmCPGy7b1IQCdHNwU7+PpVApcpBOz4DIc3+y0\nl/0P+KvRoz4cbjzcAqbUrHy7B3sLFgYZi3X1Ku7uroTsuKWf+1xzW+2UvNVtKVo2\nOjg4CMNWhsGPeGejjVbiUt3Ua49Nvx61XooCEAMXqDxBpunAi08oWVmz8qEo4X5Y\nnqLxAbgUxYcraHyzmR62+Ha6d2nmvV1xQ2OQHAfs1ONcxwaIWTOoeyjrLvU1ooCw\nPJu2XXf480Ri4L4IYNobsEZ4isTOefxEs42+f4IYXQKBgQD9uEFeoa5CbkVQ6B9S\nbA9PAiGvGinvFAM7st2mVrA9HaS1sG2aR19FokbvWcYzTETNrVBu9gpHmjsK4c7/\nn0qlKd6sdA78l1GoVXLPUCVQl9wMkXxi2xfiy+nnkF5ros1nAbLSEyUq5Ke/2Ufm\n1PthMxr62cyt3JuGAf3e15A8OwKBgQDjhzVcSCaZAjXhoxnquzvqCC/5ECX0O3NZ\nN/Od6pWy2ZfqAUpit2eQ9VrrC/q/r7bi1fgRI7xuu1sQTndF6SdRbbJ8bP+tbokS\nzyX3jcO+eV+FYN1dufxjJogeKkMhwJNcXVw6ZPEPJOUDO4gFpHLWVeR/F3k20Ml0\ni1XwyN2vDQKBgA8EY+IdAbpu2m1yf8AaI0HS53l8u/Spo5NZ/+KDiQTlB0W1vpb+\nGHZ/p2EtWBzbK8tcscEPkQYx68K2INFidUGXW9WrPOPYdP3YqOX6YWsGwgCAb/NN\n8nj7Bsos8lm+lhe9sv7aIT4LsJ4bVzUjcAmNw9ALKiE0SIRDA+q3qZLXAoGBAJJs\nsSpyRGSzx3kuPb+SPzydiEIA4mwK1nDUUTOPhEdVQS6XJgfhgAUJ4TjivLwfmNLi\nYIKnPN0GNdSIqrmTEnqU3gi1HNXADPbN2OE3moE6Gv77F3r59jLY2UCQciOGi08l\nxfTOgVP9qmQqK5919XF6VVJ1CMz+EKewA96xZfkBAoGAahiYbZCDxkrg+q539UT9\nLwjVM7kWGrwutkfbu8R8EWpbVtvPq4sXjw+5tJ/duZ+AeZZ0B6LUlPvHiWvzwyUk\n+Pd2NWnuScADxBrzRQICjCqBazGgvd0TX5pHG2AGrQ7uOpj100zkgrXK5vpa0FQH\nc+M9s8s64I6zakZpYdoBmtE=\n-----END PRIVATE KEY-----\n",
+    "client_email": "desucar@newstapa-web.iam.gserviceaccount.com",
+    "client_id": "106258591043222208812",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/desucar%40newstapa-web.iam.gserviceaccount.com"
+}
