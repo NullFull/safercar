d4e28b3677ded8cb274e66845aeccfbdf2f6a972
lexifdev <sl@lxf.kr>
2018-03-30 19:25:05 +0900

    결함 종류 설명 변경. (최종 수정 날짜 추가)

--- desucar/views.py
+++ desucar/views.py
@@ -19,23 +19,27 @@ def detail(request, maker_name, car_name, car_year, car_code):
     defects = {
         '리콜': {
             'code': 'recall',
-            'desc': '리콜은 소비자의 안전을 위협하는 중대한 결함을 바로잡기 위해 시행합니다. 이곳에서는 국토교통부 자동차리콜센터(www.car.go.kr)와 환경부가 제공한 2000년 이후 리콜 자료 전체를 보여드립니다.',
+            'desc': '리콜은 소비자의 안전을 위협하는 중대한 결함을 바로잡기 위해 시행합니다. 이곳에서는 <a href="www.car.go.kr">국토교통부 자동차리콜센터</a>와 환경부가 제공한 2000년 이후 리콜 자료 전체를 보여드립니다.',
+            'last_update': date(2018, 3, 31),
             'items': [x for x in official_defects if x.kind == 'RC'],
         },
         '무상수리': {
             'code': 'freefix',
-            'desc': '무상수리는 리콜 대상이 아닌 다양한 제작 결함을 바로잡기 위해 시행합니다. 이곳에서는 국토교통부 자동차리콜센터(www.car.go.kr)와 한국소비자원 소비자위해감시시스템(www.ciss.go.kr)에 등록된 2000년 이후 무상수리 자료 전체를 보여드립니다.',
+            'desc': '무상수리는 리콜 대상이 아닌 다양한 제작 결함을 바로잡기 위해 시행합니다. 이곳에서는 <a href="www.car.go.kr">국토교통부 자동차리콜센터</a>와 <a href="www.ciss.go.kr">한국소비자원 소비자위해감시시스템</a>에 등록된 2000년 이후 무상수리 자료 전체를 보여드립니다.',
+            'last_update': date(2018, 3, 31),
             'items': [x for x in official_defects if x.kind == 'FF'],
         },
         '비공식 결함': {
             'code': 'unofficial',
-            'desc': '정부와 제작사가 공식적으로 밝힌 리콜이나 무상수리 외에도 비공식적으로 처리되는 중요한 결함 정보가 많습니다. 이곳에서는 자동차 동호회, 인터넷 커뮤니티, 뉴스타파 제보 등을 통해 수집된 다양한 비공식 결함 정보를 알려드립니다.',
+            'desc': '공식적으로 알려진 리콜이나 무상수리 외에도 비공식적으로 처리되는 중요한 결함 정보들이 많습니다. 이곳에서는 자동차 관련 커뮤니티에서 수집된 다양한 비공식 결함 정보를 알려드립니다.',
+            'last_update': date(2018, 3, 31),
             'items': list(car.community_defects.all()),
         },
         '급발진 의심': {
             'code': 'sudden-accel',
             'desc': '2000년 이후 국토교통부와 한국소비자원에 신고된 급발진 의심 사고는 800여건이 넘지만, 지금까지 제작사나 정부가 인정한 급발진 사고는 0건입니다. 이곳에서는 차종별로 신고된 급발진 의심 사고의 구체적인 내용을 알려드립니다.',
-            'items': [x for x in car.sudden_accels.all()],
+            'last_update': date(2018, 3, 31),
+            'items': list(car.sudden_accels.all()),
         }
     }
 
